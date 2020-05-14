aeea0d6816bb68cc2763d97077385cc2d1ae1c3e
ivan-kovalevich <ivan.kovalevich.83@mail.ru>
2020-05-15 01:22:59 +0300

    Обновления класса DbHistory

--- App/Db/DbHistory.php
+++ App/Db/DbHistory.php
@@ -24,6 +24,7 @@
             $history->story_name = $story_name;
             $history->content = $content;
             \R::store($history);
+            $result = true;
         }
 
         public function updateHistory(string $id, string $story_name, string $content)
@@ -39,22 +40,31 @@
             $history->story_name = $story_name;
             $history->content = $content;
             \R::store($history);
+            $result = true;
         }
 
         public function readHistorys()
         {
-            
+            $result = \R::getAll('SELECT * FROM "unit_story"');
         }
 
-        public function readHistory()
+        public function readHistory(string $id)
         {
-            
+            $table_name = 'unit_story';
+            $model = new Model();
+            \R::ext('xdispense', function( $type ){
+                return \R::getRedBean()->dispense( $type );
+            });
+            $history = \R::xdispense($table_name);
+
+            $history = \R::load($table_name, $id);
+            $result = $history;
         }
 
         public function deleteHistory($id)
         {
 
-            $history = \R::load('unit_story', $id);
-            \R::trash($history);
+            $history = \R::exec('DELETE FROM "unit_story" WHERE id = ?, [$id]');
+            $result = true;
         }
     }
--- App/History.php
+++ App/History.php
@@ -2,12 +2,76 @@
 
 namespace App;
 
-use Core\Model;
-use Core\Application;
+use App\Db\DbHistory;
+use Db\DbAlbum;
 use Core\JWT;
-use \RedBeanPHP\RedException;
 
 class Hitory
 {
-    
+    public function createHistory()
+    {
+        $jwt = new JWT();
+        $id = $jwt->checkToken();
+        if (!empty($id) && $id != 0) {
+            $data = json_decode(file_get_contents("php://input"));
+            if (isset($data->story_name) && !empty($data->story_name)) {
+                $dbHistory = new DbHistory();
+                if (isset($data->content) && !empty($data->content)) {
+                    $content = $data->content;
+                } else {
+                    $content = null;
+                }
+                $id = $dbHistory->createHistory($id, $data->story_name, $data->content);
+                $arr = ['error' => '', 'result' => true];
+                echo json_encode($arr);
+            } else {
+                $arr = ['error' => 'Пустое поле название Истории'];
+                echo json_encode($arr);
+            }
+        } else {
+            $arr = ['error' => '', 'result' => false];
+            echo json_encode($arr);
+        }
+    }
+
+    public function getHistorys()
+    {
+        $jwt = new JWT();
+        $id = $jwt->checkToken();
+
+        if ($id != 0 && !empty($id)) {
+            $dbHistorys = new DbHistory();
+            $arr = $dbHistorys->readHistorys();
+            if ($arr != null) {
+                echo json_encode($arr);
+            } else {
+                $arr = ['error' => 'Нет записей', 'result' => false];
+                echo json_encode($arr);
+            }
+        } else {
+            $arr = ['error' => 'Токен не дествителен', 'result' => false];
+            echo json_encode($arr);
+        }
+    }
+
+    public function getHistory()
+    {
+        $jwt = new JWT();
+        $id = $jwt->checkToken();
+        $data = json_decode(file_get_contents("php://input"));// сда передовать id истории которую нужно показать
+        $idHistory = $data->id;
+        if ($id != 0 && !empty($id)) {
+            $dbHistory = new DbHistory();
+            $arr = $dbHistory->readHistory($idHistory);
+            echo json_encode($arr);
+        } else {
+            $arr = ['error' => 'Токен не дествителен', 'result' => false];
+            echo json_encode($arr);
+        }
+    }
+
+    public function updateHistory()
+    {
+        //Грач извини сделал что смог, спать хочу, уже позно да и после работы я.
+    }
 }
