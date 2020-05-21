2f6bdb9f3bd9e987281901ff39ddeb208f704217
ivan-kovalevich <ivan.kovalevich.83@mail.ru>
2020-05-22 00:55:18 +0300

    Добавление поиска

--- App/Search.php
+++ App/Search.php
@@ -24,7 +24,63 @@
             }
 
             $model = new Model();
+            $search = '%'.$data->search.'%';
 
-            $collection = \R::findCollection( 'album,');
+            $album = \R::findAll('album', 'owner_id = ? AND album_name LIKE = ?', [$id, $search]);
+            $i = 0;
+            if(!empty($album)){
+                foreach($album as $value){
+                    $array[$i]['album']['album_name'] = $value->album_name;
+                    $array[$i]['album']['id'] = $value->id;
+                    $i++;                
+                }
+            }
+            
+            $audio = \R::findAll('unit_audio', 'owner_id = ? AND audio_name LIKE = ?', [$id, $search]);
+            $i = 0;
+            if(!empty($audio)){
+                foreach($audio as $value){
+                    $array[$i]['audio']['audio_name'] = $value->audio_name;
+                    $array[$i]['audio']['id'] = $value->id;
+                    $i++;                
+                }
+            }
+
+            $photo = \R::findAll('unit_photo', 'owner_id = ? AND photo_name LIKE = ?', [$id, $search]);
+            $i = 0;
+            if(!empty($photo)){
+                foreach($photo as $value){
+                    $array[$i]['photo']['photo_name'] = $value->photo_name;
+                    $array[$i]['photo']['id'] = $value->id;
+                    $i++;                
+                }
+            }
+
+            $story = \R::findAll('unit_story', 'owner_id = ? AND story_name LIKE = ?', [$id, $search]);
+            $i = 0;
+            if(!empty($story)){
+                foreach($story as $value){
+                    $array[$i]['story']['story_name'] = $value->story_name;
+                    $array[$i]['story']['id'] = $value->id;
+                    $i++;                
+                }
+            }
+
+            $video = \R::findAll('unit_video', 'owner_id = ? AND video_name LIKE = ?', [$id, $search]);
+            $i = 0;
+            if(!empty($video)){
+                foreach($video as $value){
+                    $array[$i]['video']['video_name'] = $value->video_name;
+                    $array[$i]['video']['id'] = $value->id;
+                    $i++;                
+                }
+            }
+
+            if(!empty($array) && is_array($array)){
+                $arr = ['conten' => $array, 'result' => true];
+            } else {
+                $arr = ['error' => '', 'result' => true];
+            }
+
         }
     }
